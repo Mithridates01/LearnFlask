d3153e20de9cd9811bf4be288c877ae8b076369a
Mithridates01 <jclucas21@gmail.com>
2017-08-06 11:18:03 -0400

    start project from here

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -4,6 +4,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+
 app = Flask(__name__) # create application instance
 app.config.from_object(__name__) #loading config from file
 
@@ -17,8 +18,49 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
-    """ Database connection """
+    """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.cli.command('initdb')
+def initdb_command():
+    """ Initialize DB """
+    init_db()
+    print("Database Initialized")
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close DB at the end of request """
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+
+
+
 
